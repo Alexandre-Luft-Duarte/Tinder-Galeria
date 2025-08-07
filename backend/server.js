b4227d112fd15c430164e5094c7411181e8e6620
Alexandre Luft Duarte <alexandre@flexpro.com.br>
2025-08-07 09:47:01 -0300

    funções para requisições prontas

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+
+app.get("/usuarios", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+})
+
 app.post("/usuarios", async (req, res) => {
     await prisma.user.create({
         data: {
@@ -13,7 +19,29 @@ app.post("/usuarios", async (req, res) => {
             password: req.body.password
         }
     })
-    res.status(200).json(req.body);
+    res.status(201).json(req.body); 
+})
+
+app.put("/usuarios/:id", async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            password: req.body.password     
+        }
+    })
+    res.status(200).json(req.body); 
+})
+
+app.delete("/usuarios/:id", async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message: "Usuários excluido com sucesso!"});
 })
 
 app.listen(3000)
