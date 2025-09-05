9b0a412f01f0c7c80455a5b1386878fb26ab579c
Alexandre Luft Duarte <alexandre@flexpro.com.br>
2025-09-05 17:57:31 -0300

    funcionalidade de logout pronta. Começando ajustes de css da navbar

--- frontend/src/components/navbar/Nav.tsx
+++ frontend/src/components/navbar/Nav.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import styles from "./Nav.module.css";
-// import { logout } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 
 export default function Nav() {
     
-    // const navigate = useNavigate();
-    // function handleClick(){
-    //     logout();
-    //     navigate("/login");
-    // }
+    const navigate = useNavigate();
+    const { logout } = useAuth();
+    function handleClick(){
+        logout();
+        navigate("/login");
+    }
 
     return(
         <div className={styles.containerNav}>
             <div className={styles.linksContainer}>
                 <Link to="/about">About</Link> 
                 <Link to="/contact">Contact</Link>
-                {/* <Link to="/login" onClick={handleClick}>LogOut</Link> */}
+                <Link to="/login" onClick={handleClick}>LogOut</Link>
             </div>
             <div className={styles.titleContainer}>
                 <p>TINDER DA GALERIA</p>
